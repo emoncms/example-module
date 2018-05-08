e8b7b6bfc5e5d0144f30371df235772ae53e350b
TrystanLea <glyn.hudson@openenergymonitor.org>
2018-05-08 18:12:22 +0100

    readme

---
+++ readme.md
@@ -0,0 +1,8 @@
+# A basic example emoncms module with sidebar
+
+![example.png](example.png)
+
+### Installation
+
+    cd /var/www/emoncms/Modules
+    git clone https://github.com/emoncms/example-module.git example
--- sidebar.js
+++ sidebar.js
@@ -41,8 +41,8 @@ function show_sidebar() {
     var width = $(window).width();
     sidebar_visible = true;
     $(".sidenav").css("left","250px");
-    // if (width>=max_wrapper_width) $("#wrapper").css("padding-left","250px");
-    // $("#wrapper").css("margin","0");
+    if (width>=max_wrapper_width) $("#wrapper").css("padding-left","250px");
+    $("#wrapper").css("margin","0");
     $("#sidenav-open").hide();
     $("#sidenav-close").hide();
 }
